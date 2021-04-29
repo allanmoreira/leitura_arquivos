6a6445b424a7c5de010c3426a7c7c54451a20db7
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-04-29 17:12:10 -0300

    Parametro nome arquivo

--- src/main/java/br/com/moreirallan/manipulacaoarquivos/controller/Base64Controller.java
+++ src/main/java/br/com/moreirallan/manipulacaoarquivos/controller/Base64Controller.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("base64")
@@ -19,7 +16,7 @@ public class Base64Controller {
     private DownloadService downloadService;
 
     @RequestMapping(path = "download", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<ByteArrayResource> toCsvDownload(@RequestBody String base64) {
-        return downloadService.download(Base64Utils.fromBase64(base64), null);
+    public ResponseEntity<ByteArrayResource> toCsvDownload(@RequestBody String base64, @RequestParam (name = "nome_arquivo", required = false) String nomeArquivo) {
+        return downloadService.download(Base64Utils.fromBase64(base64), nomeArquivo);
     }
 }
